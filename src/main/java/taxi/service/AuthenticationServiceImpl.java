04239b36c058f7401144d551b10611c6d5211330
LinkClink <shekaqwe@gmail.com>
2022-03-28 16:22:40 +0300

    add logger / update ConnectionUtil

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,5 +1,7 @@
 package taxi.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Injector;
 import taxi.lib.Service;
@@ -7,6 +9,7 @@ import taxi.model.Driver;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
     private static final Injector injector = Injector.getInstance("taxi");
     private final DriverService driverService
             = (DriverService) injector.getInstance(DriverService.class);
@@ -15,8 +18,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public Driver login(String login, String password) throws AuthenticationException {
         Driver exitDriver = driverService.findByLogin(login);
         if (exitDriver != null && exitDriver.getPassword().equals(password)) {
+            logger.info("Success login with login: {}", login);
             return exitDriver;
         }
+        logger.warn("Authentication failed with login: {}", login);
         throw new AuthenticationException("Wrong password or login");
     }
 }
