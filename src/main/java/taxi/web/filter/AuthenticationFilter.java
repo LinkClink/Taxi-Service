91cbcc2724381eccaa6f1f39f27a1bf00e3c5c3d
LinkClink <shekaqwe@gmail.com>
2022-03-29 18:32:05 +0300

    rename

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -19,7 +19,7 @@ public class AuthenticationFilter implements Filter {
     private static final Set<String> allowedUrl = new HashSet<>();
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
         allowedUrl.add("/login");
         allowedUrl.add("/drivers/add");
     }
